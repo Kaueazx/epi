ec101fe7c288e9e04e9449874371af915002cf26
Kauea <kaueaexzplosion@hotmail.com>
2022-05-12 10:56:08 -0300

    iniciando formularios

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// let zuario;
+// let button;
+// button = document.getElementById("btnEnviar");
+// button.addEventListener('click', (e) => {
+// e.preventDefault();
+// zuario = document.getElementById("zuario").value;
+// alert(zuario);
+// });
+
+// $("#btnEnviar").on('click', (e) => {
+// e.preventDefault();
+// alert($("#zuario").val());
+// })
